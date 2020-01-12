3b92456ef810a71540a0611187fc1b3f1bbf32b6
Carlo Giustini <cgiustini1@gmail.com>
2020-01-11 19:53:53 -0800

    parsing works

--- effectspedal.py
+++ effectspedal.py
@@ -1,11 +1,36 @@
 import numpy as np
-from pydub import AudioSegment
-from pydub.utils import get_array_type
+import matplotlib.pyplot as plt
+import IPython
+import wave
+import struct
+import array
+import os
 
-path_to_file = "C:\\Users\\Carlo Giustini\\effectspedal\\bassnotes.mp3"
-sound = AudioSegment.from_file(file=path_to_file)
-left = sound.split_to_mono()[0]
+wav_fname = "./bassnotes.wav"
 
+# wave_read = wave.open(wav_fname)
+# n = wave_read.getnframes()
+# data = wave_read.readframes(n)
+
+# def read_whole(filename):
+#     wav_r = wave.open(filename, 'r')
+#     ret = []
+#     while wav_r.tell() < wav_r.getnframes():
+#         decoded = struct.unpack("<h", wav_r.readframes(2))
+#         ret.append(decoded)
+#     return ret
+
+# data = read_whole(wav_fname)
+
+sizes = {1: 'B', 2: 'h', 4: 'i'}
+wav = wave.open(wav_fname)
+channels = wav.getnchannels()
+fmt_size = sizes[wav.getsampwidth()]
+fmt = "<" + fmt_size * channels
+a = array.array(fmt_size)
+a.fromfile(open(wav_fname, 'rb'), int(os.path.getsize(wav_fname)/a.itemsize))
+
+IPython.embed()
 
 # Read audio of a bass note being played.
 
