cde48ee54e73f29f51cfb48724cde2475ad11684
Carlo Giustini <cgiustini1@gmail.com>
2020-01-13 15:26:15 -0800

    got basic stuff working...

--- effectspedal.py
+++ effectspedal.py
@@ -8,8 +8,8 @@ import os
 from playsound import playsound
 import scipy.signal as signal
 
-wav_fname = "./bassnotes.wav"
-owav_fname = "./bassnotes_one.wav"
+wav_fname = "./bassnotes_one.wav"
+owav_fname = "./bassnotes_one_output.wav"
 
 # wave_read = wave.open(wav_fname)
 # n = wave_read.getnframes()
@@ -33,12 +33,19 @@ sampwidth = wav.getsampwidth()
 samprate = wav.getframerate()
 fmt_size = sizes[sampwidth]
 fmt = "<" + fmt_size * channels
-a = array.array(fmt_size)
-a.fromfile(open(wav_fname, 'rb'), int(os.path.getsize(wav_fname)/a.itemsize))
-a = a.tolist()
+_data = array.array(fmt_size)
+_data.fromfile(open(wav_fname, 'rb'), int(os.path.getsize(wav_fname)/_data.itemsize))
+_data = _data.tolist()
 
 # Get one note.
-idata = a[150000:475000]
+idata = _data
+N = len(idata)
+# idata = a[150000:475000]
+
+fidx = np.arange(0, N)
+f = fidx * samprate * (1.0 / float(N))
+w = 2 * np.pi * f
+wn = fidx * (1.0 / float(N)) * 2 * np.pi
 
 framesize = 64
 frames = []
@@ -53,24 +60,22 @@ for i in np.arange(50000, 50100):
 	iframes.append(frame)
 	oframes.append(signal.lfilter([1.0], [1.0], frame))
 
-N = len(idata)
-# w = (2 * np.pi / float(N)) * np.arange(0, N)
-w = np.arange(0, N)
-b, a = signal.butter(3, [0.2, 0.8], 'bandpass')
-w, h = signal.freqs(b, a, worN=w)
+
+# Process
+b, a = signal.butter(2, 0.006, 'high')
+# b, a = signal.butter(2, 0.1, 'high')
+w, h = signal.freqz(b, a, worN=wn)
 
 
 odata = signal.lfilter(b, a, idata)
 
-plt.plot(w, np.abs(np.fft.fft(idata)) / np.max(np.abs(np.fft.fft(idata))))
+plt.plot(wn, np.abs(np.fft.fft(idata)) / np.max(np.abs(np.fft.fft(idata))), '+-')
 plt.plot(w, np.abs(np.fft.fft(odata)) / np.max(np.abs(np.fft.fft(odata))))
-plt.plot(w, np.abs(h) / np.max(abs(h)))
+plt.plot(w, np.abs(h) / np.max(abs(h)), '+-')
 
 # idx = 50000
 
 
-# Process
-# odata = idata
 
 
 # Write osginal to output.
